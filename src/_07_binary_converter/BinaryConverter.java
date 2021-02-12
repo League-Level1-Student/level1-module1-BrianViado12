c27fee50acae7643721e54740bd728ede0490cdc
Daddy <Daddy@vivs-macpro-13>
2021-02-11 17:21:27 -0800

    did this 2/11

--- src/_07_binary_converter/BinaryConverter.java
+++ src/_07_binary_converter/BinaryConverter.java
@@ -1,13 +1,60 @@
 package _07_binary_converter;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class BinaryConverter {
-	public static void main(String[] args) {
-		JFrame frame = new JFrame();
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class BinaryConverter implements ActionListener {
+	JFrame frame = new JFrame();
+	JTextField text = new JTextField(15);
+	JLabel label = new JLabel();
+	public void showButton() {
 		frame.setVisible(true);
 		JPanel panel = new JPanel();
+		frame.add(panel);
+		panel.add(text);
+		panel.add(label);
+		JButton button = new JButton();
+		panel.add(button);
+		frame.pack();
+		button.addActionListener(this);
 		
+	}
+	String convert(String input) {
+	    if(input.length() != 8){
+	        JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+	        return "-";
+	    }
+	    String binary = "[0-1]+";//must contain numbers in the given range
+	    if (!input.matches(binary)) {
+	        JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+	        return "-";   
+	    }
+	    try {
+	        int asciiValue = Integer.parseInt(input, 2);
+	        char theLetter = (char) asciiValue;
+	        return "" + theLetter;
+	    } catch (Exception e) {
+	        JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+	        return "-";
+	    }
+	}
+
+	                                       
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String answer = text.getText();
+		System.out.println(answer);
+		answer = convert(answer);
+		label.setText(answer);
+		frame.pack();
 	}
 }
